e1a2b86ff55b1c9c29341f6d2a8b6f766a973c0d
Jackson Nestelroad <jackson@nestelroad.com>
2023-06-29 23:06:50 -0700

    Add config files

---
+++ eslint/base.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+
+  extends: [
+    'eslint-config-airbnb-base/rules/errors',
+    'eslint-config-airbnb-base/rules/node',
+    'eslint-config-airbnb-base/rules/style',
+    'eslint-config-airbnb-base/rules/variables',
+    'eslint-config-airbnb-base/rules/es6',
+    'prettier',
+  ],
+
+  env: {
+    browser: true,
+  },
+
+  plugins: ['import', 'prettier'],
+
+  rules: {
+    'max-len': [
+      'error',
+      {
+        code: 120,
+      },
+    ],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-unused-vars': ['off'],
+    'no-nested-ternary': ['off'],
+    'no-underscore-dangle': ['off'],
+    'object-curly-spacing': ['error', 'always'],
+    'require-jsdoc': ['off'],
+    'no-invalid-this': ['off'],
+    'no-continue': ['off'],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: false,
+      },
+    ],
+    'class-methods-use-this': ['off'],
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+        singleQuote: true,
+        arrowParens: 'avoid',
+      },
+    ],
+  },
+};
---
+++ eslint/react.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: ['./base', 'eslint-config-airbnb/rules/react', 'plugin:react/recommended'],
+
+  rules: {
+    'react/prop-types': 'error',
+    'react/require-default-props': 'error',
+
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx', '.jsx'] }],
+    'react/prefer-stateless-function': 'off',
+    'react/jsx-no-bind': 'error',
+    'react/sort-comp': [
+      'error',
+      {
+        order: [
+          'displayName',
+          'statics',
+          'static-methods',
+          'defaultProps',
+          'state',
+          'constructor',
+          'render',
+          '/^(_)?render.+$/',
+          'componentWillMount',
+          'componentDidMount',
+          'componentWillReceiveProps',
+          'shouldComponentUpdate',
+          'componentWillUpdate',
+          'componentDidUpdate',
+          'componentWillUnmount',
+          '/^on[A-Z].+$/',
+          'everything-else',
+          '/^_.+$/',
+        ],
+      },
+    ],
+  },
+};
